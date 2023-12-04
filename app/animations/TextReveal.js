8d3b062fb88e1e406f2467cad50ca222a6e1f217
Harun-Mac <iamharunjonuzi@gmail.com>
2023-12-04 15:22:53 +0100

    feat: added text animations

---
+++ app/animations/TextReveal.js
@@ -0,0 +1,27 @@
+import { gsap } from "gsap";
+import SplitType from "split-type";
+import Observer from "../classes/Observer";
+
+export default class TextReveal extends Observer {
+   constructor({ element }) {
+      super({ element: element });
+      this.element = element;
+
+      const splitted = new SplitType(this.element, {
+         types: "words",
+      });
+
+      this.splitWords = new SplitType(splitted.words, {
+         types: "words",
+      });
+   }
+
+   onEnter() {
+      gsap.to(this.splitWords.words, {
+         y: "0%",
+         duration: 1.3,
+         stagger: 0.05,
+         ease: "power3inOut",
+      });
+   }
+}
--- app/main.js
+++ app/main.js
@@ -1,1 +1,39 @@
 import "../src/scss/style.scss";
+import Lenis from "@studio-freight/lenis";
+import TextReveal from "./animations/TextReveal";
+
+class App {
+   constructor() {
+      // "_" convention means it's a private method, and it should not be called outside this method
+      this._createLenis();
+      this._createTextReveals();
+      this._render();
+   }
+
+   _createTextReveals() {
+      const textItems = [
+         ...document.querySelectorAll('[data-animation="text-reveal"]'),
+      ];
+
+      textItems.forEach((text) => {
+         new TextReveal({
+            element: text,
+         });
+      });
+   }
+
+   _createLenis() {
+      this.lenis = new Lenis({
+         lerp: 0.1,
+      });
+   }
+
+   _render(time) {
+      // request animation frame
+      this.lenis.raf(time);
+
+      requestAnimationFrame(this._render.bind(this));
+   }
+}
+
+new App();
